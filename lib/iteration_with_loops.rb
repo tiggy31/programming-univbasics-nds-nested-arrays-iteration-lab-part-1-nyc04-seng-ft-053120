87d752209929d476ebc4357a0f7e538cbe23de36
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-08 19:51:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,5 +3,11 @@ def find_even_values(src)
   while outer_index < src.count do
     inner_index = 0
      while inner_index < src[outer_index].count do
-       if src
+       if src[outer_index][inner_index].even?
+         p src[outer_index][inner_index]
+       end
+       inner_index += 1
+     end
+     row_index+=1
+   end
 end
